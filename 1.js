a3c9996ca37531ca10423d066e0bb32d3ca6d8c1
Mohamed Abdallah <mo.ab.elkasaby@gmail.com>
2017-04-17 20:10:57 +0200

    Add 1.js

--- 1.js
+++ 1.js
@@ -1,1 +1,20 @@
-// write some node code finally!
+#!/usr/bin/env node
+
+function printHelp(){
+	console.log("1.js (c) MoAbd")
+	console.log("");
+	console.log("usage:");
+	console.log("--help            print this help");
+	console.log("--name={NAME}     print Hello {NAME}");
+	console.log("");
+}
+
+var args = require("minimist")(process.argv.slice(2),{ string: "name"});
+
+if (args.help || !args.name){
+	printHelp();
+	process.exit(1)
+}
+var name = args.name;
+
+console.log('Hello ' + name);
