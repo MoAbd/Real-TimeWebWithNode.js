cbcb137595bd0bcce1d3f6f684bce69911b38358
Mohamed Abdallah <mo.ab.elkasaby@gmail.com>
2017-04-20 03:48:08 +0200

    Add Asynquence instead of call-back

---
+++ 2.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+
+function printHelp(){
+	console.log("2.js (c) MoAbd")
+	console.log("");
+	console.log("usage:");
+	console.log("--help            print this help");
+	console.log("--file={NAME}     read the file of {NAME}");
+	console.log("");
+}
+
+var args = require("minimist")(process.argv.slice(2),{ string: "file"});
+
+if (args.help || !args.file){
+	printHelp();
+	process.exit(2)
+}
+
+var hello= require("./helloworld2.js");
+hello.say(args.file)
+.val(function(contents){
+	console.log(contents.toString());
+})
+.or(function(err) {
+	console.error(err);
+});
+
